5724439498ba4169b0d4f9137fb08827931b9fb3
zfanta <jht008@gmail.com>
2021-12-03 20:48:59 +0900

    test

--- App.tsx
+++ App.tsx
@@ -8,108 +8,78 @@
  * @format
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Linking, SafeAreaView, Text} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {launchImageLibrary} from 'react-native-image-picker';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+const url = 'https://api-dev.jdjeon.com';
+
+async function login() {
+  await fetch(`${url}/auth/signIn`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: '1@user.com',
+      password: '1',
+    }),
+  });
+}
+
+async function getUploadUrl(): Promise<{
+  [name: string]: {url: string; key: string};
+}> {
+  const response = await fetch(
+    `${url}/aws/s3/pre-signed-upload-request?filenames=test.jpg`,
+    {
+      method: 'GET',
+      credentials: 'include',
+    },
   );
-};
+  return await response.json();
+}
+
+async function getImageBlob() {
+  const image = await launchImageLibrary({mediaType: 'photo'});
+
+  const {uri} = image.assets?.[0] ?? {};
+  if (uri === undefined) {
+    throw new Error('No image selected');
+  }
+
+  return await (await fetch(uri)).blob();
+}
+
+async function uploadImage() {
+  await login();
+  const {url: uploadUrl, key} = (await getUploadUrl())['test.jpg'];
+
+  const imageBlob = await getImageBlob();
+
+  await fetch(uploadUrl, {
+    method: 'PUT',
+    body: imageBlob,
+  });
+
+  return key;
+}
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [key, setKey] = useState<string>('');
+  useEffect(() => {
+    uploadImage().then(setKey).catch(console.error);
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <Text onPress={() => Linking.openURL(`https://image.jdjeon.com/${key}`)}>
+        {`https://image.jdjeon.com/${key}`}
+      </Text>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
