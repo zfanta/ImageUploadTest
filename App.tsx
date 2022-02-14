496cd51549f1e026c9f3ff8f08ca0d7815488613
zfanta <jht008@gmail.com>
2022-02-14 16:22:36 +0900

    react-native-image-crop-picker

--- App.tsx
+++ App.tsx
@@ -12,14 +12,28 @@ import React from 'react';
 import {Button, SafeAreaView, ScrollView, View, Text} from 'react-native';
 
 import MultipleImagePicker from '@baronha/react-native-multiple-image-picker';
+import ImagePicker from 'react-native-image-crop-picker';
 
 const App = () => {
   const [images, setImages] = React.useState<string[]>([]);
+  const [image, setImage] = React.useState<string>('');
 
-  function onPress() {
+  function onPickPress() {
     MultipleImagePicker.openPicker({})
       .then(response => {
         setImages(response.map(asset => asset.path));
+      })
+      .catch(console.error);
+  }
+
+  function onCameraPress() {
+    ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+    })
+      .then(response => {
+        setImage(response.path);
       })
       .catch(console.error);
   }
@@ -28,12 +42,14 @@ const App = () => {
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
-          <Button title="Pick" onPress={onPress} />
+          <Button title="Pick" onPress={onPickPress} />
           {images.map(image => (
             <View key={image}>
               <Text>{image}</Text>
             </View>
           ))}
+          <Button title="Camera" onPress={onCameraPress} />
+          <Text>{image}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
